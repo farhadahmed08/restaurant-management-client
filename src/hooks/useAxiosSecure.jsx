9c133488d01a8add9632b4feb9c80c70b44a8c34
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-01 16:07:13 +0800

    refector the project

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,33 +3,33 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
-
-
 export const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
-    withCredentials:true
-})
+  baseURL: "http://localhost:5000",
+  withCredentials: true,
+});
 
 const useAxiosSecure = () => {
-    const {logOut}=  useAuth()
-    const navigate = useNavigate();
-    useEffect(()=>{
-        axiosSecure.interceptors.response.use(res=>{
-            return res;
-        },error=>{
-            console.log('error tracked in the interceptor',error.response);
-            if (error.response.status === 401 || error.response.status === 403) {
-                console.log('logout the user')
-                logOut()
-        .then(()=>{
-            navigate('/login')
-        })
-        .catch(error=>console.log(error))
-            }
-        })
-        },[logOut,navigate])
-    return axiosSecure;
-   
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (error) => {
+        console.log("error tracked in the interceptor", error.response);
+        if (error.response.status === 401 || error.response.status === 403) {
+          console.log("logout the user");
+          logOut()
+            .then(() => {
+              navigate("/login");
+            })
+            .catch((error) => console.log(error));
+        }
+      }
+    );
+  }, [logOut, navigate]);
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
