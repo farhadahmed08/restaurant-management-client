0b31cd40c27ae0dd20ca451a40977144d0c1cbe4
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-12-01 18:00:31 +0800

    error paage modified

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -6,11 +6,28 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
     return (
         <div>
-            <h2>this is error page</h2>
-            <Link to='/'>
-              <button className="btn btn-sm  btn-primary">Login</button>
-          </Link>
+      <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          <div className="max-w-md text-center">
+            <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+              <span className="sr-only">Error</span>404
+            </h2>
+            <p className="text-2xl font-semibold md:text-3xl">
+              Sorry, we could not find this page.
+            </p>
+            <p className="mt-4 mb-8 dark:text-gray-400">
+              But do not worry, you can find plenty of other things on our
+              homepage.
+            </p>
+           
+             <Link to="/"> <button className="btn btn-primary">Back to homepage</button></Link>
+
+             
+           
+          </div>
         </div>
+      </section>
+    </div>
     );
 };
 
